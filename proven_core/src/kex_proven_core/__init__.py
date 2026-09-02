b181da30788e80968d9e1ad4520c2df1f3026a9c
aboudykeddeh276-stack <aboudykeddeh276@gmail.com>
2026-09-02 17:54:16 +0930

    Add KEX proven core package exports

---
+++ proven_core/src/kex_proven_core/__init__.py
@@ -0,0 +1,16 @@
+from .core import (
+    EvidenceState,
+    KEXAdmission,
+    LogicalObject,
+    Mutation,
+    ProofLedger,
+    PromotionGate,
+    Registry,
+    Surface,
+    build_minframe,
+)
+
+__all__ = [
+    "EvidenceState", "KEXAdmission", "LogicalObject", "Mutation", "ProofLedger",
+    "PromotionGate", "Registry", "Surface", "build_minframe",
+]
