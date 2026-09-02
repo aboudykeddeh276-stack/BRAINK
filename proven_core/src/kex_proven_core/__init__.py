d6c70ca4f9818f2d5f005b3e516c6c36b083664b
aboudykeddeh276-stack <aboudykeddeh276@gmail.com>
2026-09-02 17:56:44 +0930

    Expose proven runtime orchestration primitives

--- proven_core/src/kex_proven_core/__init__.py
+++ proven_core/src/kex_proven_core/__init__.py
@@ -9,8 +9,10 @@ from .core import (
     Surface,
     build_minframe,
 )
+from .orchestrator import AdapterResult, Capability, DurableRuntimeRegistry, Route, Supervisor
 
 __all__ = [
     "EvidenceState", "KEXAdmission", "LogicalObject", "Mutation", "ProofLedger",
     "PromotionGate", "Registry", "Surface", "build_minframe",
+    "AdapterResult", "Capability", "DurableRuntimeRegistry", "Route", "Supervisor",
 ]
