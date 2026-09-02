5335694fa0bd31659ca2e2ec62435edae3f33531
aboudykeddeh276-stack <aboudykeddeh276@gmail.com>
2026-09-02 10:51:23 +0930

    deploy: install R16 genome orchestrator

---
+++ enterprise/genome_orchestrator.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from .service_genome import ServiceGenomeEngine
+from .server_room import ServerRoomComposer
+from .business_replication import BusinessReplicator
+
+class GenomeOrchestrator:
+    def __init__(self,catalog):
+        self.genomes=ServiceGenomeEngine(catalog)
+        self.rooms=ServerRoomComposer(catalog)
+        self.replicator=BusinessReplicator()
+
+    def provision_plan(self,undertaking,business_id,domains,region="AU-SA",scale="SMALL",extra_genes=None):
+        genome=self.genomes.genome(undertaking,extra_genes)
+        room=self.rooms.compose(genome,scale)
+        replication=self.replicator.replicate(room,business_id,domains,region)
+        return {"genome":genome,"room":room,"replication":replication}
